0846c93ae27986a660bd1128234f826f7ef0bcf1
andry <andry@DESKTOP-SENM7HG>
2020-11-08 18:53:14 +0100

    Add project files.

---
+++ Es1/Matematica.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaMatematica
+{
+    class Matematica
+    {
+        public static int Somma(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double DoubleSomma(double n1, double n2)
+        {
+            return n1 + n2;
+        }
+
+        public static double DoubleSottrazione(double n1, double n2)
+        {
+            return n1 - n2;
+        }
+
+        public static double DoubleMoltiplicazione(double n1, double n2)
+        {
+            return n1 * n2;
+        }
+
+        public static double DoubleDivisione(double n1, double n2)
+        {
+            return n1 / n2;
+        }
+
+        public static long LongPotenza(long n1, long n2)
+        {
+            long potenza = 1;
+            for(int c = 0; c < n2; c++)
+            {
+                potenza *= n1;
+            }
+            return potenza;
+        }
+
+        public static long CalcolaFattoriale(int n1)
+        {
+            long fattoriale = 1;
+            for(int c = 2; c <= n1; c++)
+            {
+                fattoriale *= c;
+            }
+            return fattoriale;
+        }
+
+        public static int MinoreTra(int n1, int n2, int n3)
+        {
+            int nmin;
+            nmin = Math.Min(n1, Math.Min(n2, n3));
+            return nmin;
+            
+        }
+
+        public static int MaggioreTra(int n1, int n2, int n3)
+        {
+            int nmax;
+            nmax = Math.Max(n1, Math.Max(n2, n3));
+            return nmax;
+        }
+
+        public static double Inverso(double n1)
+        {
+            double inverso; 
+            inverso = n1 * -1;
+            return inverso;
+        }
+
+        public static bool IsPari(int n1)
+        {
+            bool npari;
+            if(n1 % 2 == 0)
+            {
+                npari = true;
+            }
+            else
+            {
+                npari = false;
+            }
+            return npari;
+        }
+
+        public static bool IsDispari(int n1)
+        {
+            bool ndispari;
+            if(n1 % 2 == 0)
+            {
+                ndispari = false;
+            }
+            else
+            {
+                ndispari = true;
+            }
+            return ndispari;
+        }
+
+        public static bool IsPositivo(int n1)
+        {
+            bool npos;
+            if(n1 > 0)
+            {
+                npos = true;
+            }
+            else
+            {
+                npos = false;
+            }
+            return npos;
+        }
+
+        public static bool IsNegativo(int n1)
+        {
+            bool nneg;
+            if(n1 < 0)
+            {
+                nneg = true;
+            }
+            else
+            {
+                nneg = false;
+            }
+            return nneg;
+        }
+        
+        public static bool IsIntero(double n1)
+        {
+            bool intero;
+            if(n1 == (int) n1)
+            {
+                intero = true;
+            }
+            else
+            {
+                intero = false;
+            }
+            return intero;
+        }
+
+
+    }
+}
---
+++ Es1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using LibreriaMatematica;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int scelta = 0, scelta2 = 0;
+            Console.Write("Inserisci 1 se vuoi eseguire: la somma, la sottrazione, la moltiplicazione, la divisione o la potenza. \ninserisci 2 se vuoi eseguire: long Fatooriale, Minimo tra 3 numeri, Massimo tra 3 numeri, double Inverso. \nInserisci 3 se vuoi eseguire: Verifica se il numero è pari, Verifica se il numero è dispari, Verifica se il numero è positivo, Verifica se il numero è negativo, Verifica se il numero è intero \n \n");
+            scelta = int.Parse(Console.ReadLine());
+            if(scelta == 1)
+            {
+                Console.Write("Inserisci il numero accanto alla operazione per svolgerla:\n1 Somma \n2 double Somma \n3 double Sottrazione \n4 double Moltiplicazione \n5 double Divisione \n6 long Potenza \n");
+                scelta2 = int.Parse(Console.ReadLine());
+                if(scelta2 == 1)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    int somma = Matematica.Somma(n1, n2);
+                    Console.WriteLine($"la somma tra i due nuemri è: {somma}");
+                }
+                else if( scelta2 == 2)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    double doublesomma = Matematica.DoubleSomma(n1, n2);
+                    Console.WriteLine($"la somma double tra i due nuemri è: {doublesomma}");
+                }
+                else if( scelta2 == 3)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    double doublesottrazione = Matematica.DoubleSottrazione(n1, n2);
+                    Console.WriteLine($"la sottrazione double tra i due nuemri è: {doublesottrazione}");
+                }
+                else if(scelta2 == 4)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    double doublemoltiplicazione = Matematica.DoubleMoltiplicazione(n1, n2);
+                    Console.WriteLine($"la moltiplicazione double tra i due nuemri è: {doublemoltiplicazione}");
+                }
+                else if (scelta2 == 5)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    double doubledivisione = Matematica.DoubleDivisione(n1, n2);
+                    Console.WriteLine($"la divisione double tra i due nuemri è: {doubledivisione}");
+
+                }
+                else if(scelta2 == 6)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    long longpotenza = Matematica.LongPotenza(n1, n2);
+                    Console.WriteLine($"la potenza long tra i due nuemri è: {longpotenza}");
+                }
+                
+            }
+            else if(scelta == 2)
+            {
+                Console.Write("Inserisci il numero accanto alla operazione per svolgerla: \n1 long Fatooriale \n2 Minimo tra 3 numeri \n3 Massimo tra 3 numeri \n4 double Inverso \n");
+                scelta2 = int.Parse(Console.ReadLine());
+                if(scelta2 == 1)
+                {
+                    int n1 = LeggiNumero();
+                    long fattoriale = Matematica.CalcolaFattoriale(n1);
+                    Console.WriteLine($"Il fattoriale del numero è: {fattoriale}");
+                }
+                else if(scelta2 == 2)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    int n3 = LeggiNumero();
+                    int nmin = Matematica.MinoreTra(n1, n2, n3);
+                    Console.WriteLine($"Il minore tra i numeri è: {nmin}");
+                }
+                else if(scelta2 == 3)
+                {
+                    int n1 = LeggiNumero();
+                    int n2 = LeggiNumero();
+                    int n3 = LeggiNumero();
+                    int nmax = Matematica.MaggioreTra(n1, n2, n3);
+                    Console.WriteLine($"Il maggiore tra i numeri è: {nmax}");
+                }
+                else if(scelta2 == 4)
+                {
+                    int n1 = LeggiNumero();
+                    double inverso = Matematica.Inverso(n1);
+                    Console.WriteLine($"Il numero invertito è: {inverso}");
+                }
+
+            }
+            else if(scelta == 3)
+            {
+                Console.Write("Inserisci il numero accanto alla operazione per svolgerla:\n1 Verifica se il numero è pari \n2 Verifica se il numero è dispari \n3 Verifica se il numero è positivo \n4 Verifica se il numero è negativo \n5 Verifica se il numero è intero \n");
+                scelta2 = int.Parse(Console.ReadLine());
+                if(scelta2 == 1)
+                {
+                    int n1 = LeggiNumero();
+                    bool npari = Matematica.IsPari(n1);
+                    Console.WriteLine($"il numero è pari? {npari}");
+                }
+                else if(scelta2 == 2)
+                {
+                    int n1 = LeggiNumero();
+                    bool ndispari = Matematica.IsDispari(n1);
+                    Console.WriteLine($"il numero è dispari? {ndispari}");
+                }
+                else if(scelta2 == 3)
+                {
+                    int n1 = LeggiNumero();
+                    bool npos = Matematica.IsPositivo(n1);
+                    Console.WriteLine($"il numero è positivo? {npos}");
+                }
+                else if(scelta2 == 4)
+                {
+                    int n1 = LeggiNumero();
+                    bool nneg = Matematica.IsNegativo(n1);
+                    Console.WriteLine($"il numero è negativo? {nneg}");
+                }
+                else if(scelta2 == 5)
+                {
+                    int n1 = LeggiNumero();
+                    bool intero = Matematica.IsIntero(n1);
+                    Console.WriteLine($"il numero è intero? {intero}");
+                }
+            }
+            Console.ReadLine();
+        }
+        static int LeggiNumero()
+        {
+            Console.Write("inserisci un numero: ");
+            int n = int.Parse(Console.ReadLine());
+            return n;
+
+        }
+
+    }
+}
